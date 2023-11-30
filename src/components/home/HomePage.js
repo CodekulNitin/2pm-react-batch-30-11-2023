cb7702a4f6c22e329649b298f37ea9d8f2d488a6
nn202213 <nn202213@appystack.com>
2023-11-30 15:06:02 +0530

    first commit

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,8 +1,8 @@
 import React from "react";
 import CompanyImage from "../common/assets/abc.png";
-import IncImage from '../common/assets/IncImage.png'
-import FastTagImage from "../common/assets/FastageImage.png"
-import ElapisImage from '../common/assets/ElispseImage.png'
+import IncImage from "../common/assets/IncImage.png";
+import FastTagImage from "../common/assets/FastageImage.png";
+import ElapisImage from "../common/assets/ElispseImage.png";
 
 const dummyArr = [
   {
@@ -25,11 +25,38 @@ const dummyArr = [
   },
 ];
 
+const cardData = [
+  {
+    id: 1,
+    srNo: "01",
+    icon: "",
+    cardName: "Reminding & Resumption",
+    description:
+      "You're constantly switching between unfinished tasks all day, leaving tabs open so you don't lose where you left off.",
+  },
+  {
+    id: 2,
+    srNo: "02",
+    icon: "",
+    cardName: "Resurfacing",
+    description:
+      "You leave articles and papers open in tabs to read because you don't have time to read them right now but if you put them away you may forget and never get back to them.",
+  },
+  {
+    id: 3,
+    srNo: "03",
+    icon: "",
+    cardName: "Refinding",
+    description:
+      "You leave docs and links open to avoid sifting through emails or Slack to find them again.",
+  },
+];
+
 function HomePage() {
   return (
-    <div>
+    <div className="px-5 md:px-12">
       <div className="pt-20  bg-[url('https://uploads-ssl.webflow.com/631f9b7b3a2f7a42f4ff5280/6320c6e2297b480e45fe9561_Grad-2.svg')] text-center pb-5">
-        <h1 className="text-[72px] font-extrabold">
+        <h1 className="md:text-[50px] lg:text-[72px] font-extrabold">
           Your browser is a mess.
           <br /> It’s not your fault.
         </h1>
@@ -48,14 +75,37 @@ function HomePage() {
         {/* secition 2 */}
       </div>
       <h4 className="text-center text-gray-400 my-5">AS FEATURED IN</h4>
-      <div className="grid grid-cols-6 gap-2 px-12">
+      <div className="grid grid-cols-2 md:grid-cols-6 gap-2 ">
         {dummyArr.map((compName) => {
           return (
             <div>
-              <img src={compName.companyName} alt="png" width="120" />
+              <img
+                src={compName.companyName}
+                alt="png"
+                className="md:w-[80px] lg:w-[120px]"
+              />
             </div>
           );
         })}
+      </div>
+      <div>
+        <h1 className="md:text-2xl lg:text-5xl font-bold my-10 lg:my-20">
+          Skeema solves the three main problems that <br /> cause tab overload.
+        </h1>
+        <div className="grid md:grid-cols-3 gap-3">
+          {cardData.map((cardName) => {
+            return (
+              <div className="shadow p-3 border rounded-xl bg-white ">
+                <div className="flex justify-between">
+                  <h3>{cardName.srNo}</h3>
+                  <img src={cardName.icon} alt="image" />
+                </div>
+                <h3 className="my-3">{cardName.cardName}</h3>
+                <p>{cardName.description}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
